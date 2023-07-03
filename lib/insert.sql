3b1191bb0ea7beaa4d9a116713c15edb495c882a
Leewell-Muchiri <leewellmacharia10@gmail.com>
2023-07-03 17:19:30 +0300

    completed work through

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'Chocolate', 'cool', true);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 21, 'M', 'Black', 'cocky', true);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabittha', 19, 'F', 'Yellow', ' calm', true);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 77, 'M', 'Brown', 'composed', false);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 36, 'F', 'nice', false);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 24, 'F', 'social', true);
+-- INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 30, 'M', 'social', true);
+
+INSERT INTO bears (name) VALUES ('one'), ('two'), ('three'), ('four'), ('five'), ('six'), ('seven'), (NULL);
